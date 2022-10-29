1da44fba603a654ab2d2765467257d11b64cfcc5
vanessadecoded <vanessamayama@gmail.com>
2022-10-29 15:42:59 +0300

    login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,17 @@
+//import 'package:google_fonts/google_fonts.dart';
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+
+@override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    body: Center(
+      child: Text('HEEY LOGIN!'),
+    ),
+  );
+}
+}
